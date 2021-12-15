694d321f9d11e6e1896cd90e6ff9795e18bdadd8
JairMartinez86 <jmartinezg86@gmail.com>
2021-12-15 16:34:18 -0600

    CAMBIO

--- RocedesAPI/Global.asax.cs
+++ RocedesAPI/Global.asax.cs
@@ -7,12 +7,16 @@ using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
 
+
 namespace RocedesAPI
 {
     public class WebApiApplication : System.Web.HttpApplication
     {
         protected void Application_Start()
         {
+            Bold.Licensing.BoldLicenseProvider.RegisterLicense("kWKSry1rwRRGBjhEqCo8X3eeTTx47GhV9y2uSbPowd4=");
+        
+
             AreaRegistration.RegisterAllAreas();
             GlobalConfiguration.Configure(WebApiConfig.Register);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
