41c58f3d10356b56ba18f71d61056d9eb88e11d3
JairMartinez86 <jmartinezg86@gmail.com>
2022-03-10 16:24:59 -0600

    cambios

--- RocedesAPI/Controllers/PRM/OperacionesController.cs
+++ RocedesAPI/Controllers/PRM/OperacionesController.cs
@@ -2985,76 +2985,83 @@ namespace RocedesAPI.Controllers.INV
                     if (_Conexion.MethodAnalysis.FirstOrDefault(w => w.IdDataMachine == IdDataMachine) != null) IdMethodAnalysis = _Conexion.MethodAnalysis.Where(w => w.IdDataMachine == IdDataMachine).Max(m => m.IdMethodAnalysis);
 
 
+                    if(IdMethodAnalysis > 0)
+                    {
+                        var lst = (from q in _Conexion.MethodAnalysis
+                                   join m in _Conexion.MachineData on q.IdDataMachine equals m.IdDataMachine
+                                   where q.IdMethodAnalysis == 0
+                                   select new
+                                   {
+                                       IdMethodAnalysis = q.IdMethodAnalysis,
+                                       Codigo = q.Codigo,
+                                       Operacion = q.Operacion,
+                                       Rate = q.Rate,
+                                       JornadaLaboral = q.JornadaLaboral,
+                                       IdManufacturing = q.IdManufacturing,
+                                       Manufacturing = q.Manufacturing,
+                                       IdProducto = q.IdProducto,
+                                       Producto = q.Producto,
+                                       IdFamily = q.IdFamily,
+                                       Family = q.Family,
+                                       IdSecuence = q.IdSecuence,
+                                       Secuence = q.Secuence,
+                                       IdDataMachine = q.IdDataMachine,
+                                       DataMachine = q.DataMachine,
+                                       Machine = m.Machine,
+                                       Delay = q.Delay,
+                                       Personal = q.Personal,
+                                       Fatigue = q.Fatigue,
+                                       IdStitchType = q.IdStitchType,
+                                       TypeStitch = q.TypeStitch,
+                                       IdNeedle = q.IdNeedle,
+                                       NeedleType = q.NeedleType,
+                                       IdRpm = q.IdRpm,
+                                       Rpm = q.Rpm,
+                                       IdStitchInch = q.IdStitchInch,
+                                       StitchInch = q.StitchInch,
+                                       IdTela = q.IdTela,
+                                       Tela = q.Tela,
+                                       IdOunce = q.IdOunce,
+                                       Ounce = q.Ounce,
+                                       IdCaliber = q.IdCaliber,
+                                       Caliber = q.Caliber,
+                                       IdFeedDog = q.IdFeedDog,
+                                       FeedDog = q.FeedDog,
+                                       IdPresserFoot = q.IdPresserFoot,
+                                       PresserFoot = q.PresserFoot,
+                                       IdFolder = q.IdFolder,
+                                       Folder = q.Folder,
+                                       MateriaPrima_1 = q.MateriaPrima_1,
+                                       MateriaPrima_2 = q.MateriaPrima_2,
+                                       MateriaPrima_3 = q.MateriaPrima_3,
+                                       FechaRegistro = q.FechaRegistro,
+                                       IdUsuario = q.IdUsuario,
+                                       Usuario = _Conexion.Usuario.First(u => u.IdUsuario == q.IdUsuario).Login,
+                                       FactorSewing = q.FactorSewing,
+                                       FactorSewingAccuracy = q.FactorSewingAccuracy,
+                                       Sewing = q.Sewing,
+                                       Tmus_Mac = q.Tmus_Mac,
+                                       Tmus_MinL = q.Tmus_MinL,
+                                       Min_Mac = q.Min_Mac,
+                                       Min_NML = q.Min_NML,
+                                       Min_Mac_CC = q.Min_Mac_CC,
+                                       Min_NML_CC = q.Min_NML_CC,
+                                       Sam = q.Sam,
+                                       ProducJL = q.ProducJL,
+                                       Precio = q.Precio,
+                                       IdUsuarioModifica = q.IdUsuarioModifica,
+                                       UsuarioModifica = (q.IdUsuarioModifica == null ? string.Empty : _Conexion.Usuario.First(u => u.IdUsuario == (int)q.IdUsuarioModifica).Login),
+                                       FechaModifica = q.FechaModifica,
+                                   }).ToList();
 
-                    var lst = (from q in _Conexion.MethodAnalysis
-                               join m in _Conexion.MachineData on q.IdDataMachine equals m.IdDataMachine
-                               where q.IdMethodAnalysis == 0
-                               select new
-                               {
-                                   IdMethodAnalysis = q.IdMethodAnalysis,
-                                   Codigo = q.Codigo,
-                                   Operacion = q.Operacion,
-                                   Rate = q.Rate,
-                                   JornadaLaboral = q.JornadaLaboral,
-                                   IdManufacturing = q.IdManufacturing,
-                                   Manufacturing = q.Manufacturing,
-                                   IdProducto = q.IdProducto,
-                                   Producto = q.Producto,
-                                   IdFamily = q.IdFamily,
-                                   Family = q.Family,
-                                   IdSecuence = q.IdSecuence,
-                                   Secuence = q.Secuence,
-                                   IdDataMachine = q.IdDataMachine,
-                                   DataMachine = q.DataMachine,
-                                   Machine = m.Machine,
-                                   Delay = q.Delay,
-                                   Personal = q.Personal,
-                                   Fatigue = q.Fatigue,
-                                   IdStitchType = q.IdStitchType,
-                                   TypeStitch = q.TypeStitch,
-                                   IdNeedle = q.IdNeedle,
-                                   NeedleType = q.NeedleType,
-                                   IdRpm = q.IdRpm,
-                                   Rpm = q.Rpm,
-                                   IdStitchInch = q.IdStitchInch,
-                                   StitchInch = q.StitchInch,
-                                   IdTela = q.IdTela,
-                                   Tela = q.Tela,
-                                   IdOunce = q.IdOunce,
-                                   Ounce = q.Ounce,
-                                   IdCaliber = q.IdCaliber,
-                                   Caliber = q.Caliber,
-                                   IdFeedDog = q.IdFeedDog,
-                                   FeedDog = q.FeedDog,
-                                   IdPresserFoot = q.IdPresserFoot,
-                                   PresserFoot = q.PresserFoot,
-                                   IdFolder = q.IdFolder,
-                                   Folder = q.Folder,
-                                   MateriaPrima_1 = q.MateriaPrima_1,
-                                   MateriaPrima_2 = q.MateriaPrima_2,
-                                   MateriaPrima_3 = q.MateriaPrima_3,
-                                   FechaRegistro = q.FechaRegistro,
-                                   IdUsuario = q.IdUsuario,
-                                   Usuario = _Conexion.Usuario.First(u => u.IdUsuario == q.IdUsuario).Login,
-                                   FactorSewing = q.FactorSewing,
-                                   FactorSewingAccuracy = q.FactorSewingAccuracy,
-                                   Sewing = q.Sewing,
-                                   Tmus_Mac = q.Tmus_Mac,
-                                   Tmus_MinL = q.Tmus_MinL,
-                                   Min_Mac = q.Min_Mac,
-                                   Min_NML = q.Min_NML,
-                                   Min_Mac_CC = q.Min_Mac_CC,
-                                   Min_NML_CC = q.Min_NML_CC,
-                                   Sam = q.Sam,
-                                   ProducJL = q.ProducJL,
-                                   Precio = q.Precio,
-                                   IdUsuarioModifica = q.IdUsuarioModifica,
-                                   UsuarioModifica = (q.IdUsuarioModifica == null ? string.Empty : _Conexion.Usuario.First(u => u.IdUsuario == (int)q.IdUsuarioModifica).Login),
-                                   FechaModifica = q.FechaModifica,
-                               }).ToList();
+                        json = Cls.Cls_Mensaje.Tojson(lst, lst.Count, string.Empty, string.Empty, 0);
+
+                    }
+                    else
+                        json = Cls.Cls_Mensaje.Tojson(null, 0, string.Empty, string.Empty, 0);
 
 
-                    json = Cls.Cls_Mensaje.Tojson(lst, lst.Count, string.Empty, string.Empty, 0);
+
 
 
                 }
